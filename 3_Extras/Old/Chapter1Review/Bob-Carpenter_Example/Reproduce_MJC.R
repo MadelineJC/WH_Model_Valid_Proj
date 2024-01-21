df3a20b86f47fc650ce9c330fea829991b12091a
Madeline Jarvis-Cross <51466332+MadelineJC@users.noreply.github.com>
2024-01-21 10:33:15 -0500

    Content moved to "TroubleShooting_2"

--- 3_Extras/Old/Chapter1Review/Bob-Carpenter_Example/Reproduce_MJC.R
+++
@@ -1,8 +0,0 @@
-# Read in data
-lynx_hare_df <- read.csv("3_Extras/Old/Chapter1Review/Bob-Carpenter_Example/hudson-bay-lynx-hare.csv")
-
-# Plotting
-plot(lynx_hare_df$Year, lynx_hare_df$Lynx, type = "l", xlab = "Year", ylab = "Population Abundance", ylim = c(0, max(lynx_hare_df$Hare)), col = "firebrick2", lwd = 2)
-lines(lynx_hare_df$Year, lynx_hare_df$Hare, col = "orchid", lwd = 2)
-legend("topright", legend = c("Lynx", "Hare"), col = c("firebrick2", "orchid"), lwd = 2)
-
